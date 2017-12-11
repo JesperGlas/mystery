a175a6a9a672c19df1174d4203653715283399a2
JesperGlas <jesper.glas@gmail.com>
2017-12-11 20:19:17 +0100

    Added docs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  text:any = {
+    Year: '',
+    Month: '',
+    Weeks: "",
+    Days: "",
+    Hours: "",
+    Minutes: "",
+    Seconds: "",
+    MilliSeconds: ""
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { CountDown } from 'ng2-date-countdown';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CountDown
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
